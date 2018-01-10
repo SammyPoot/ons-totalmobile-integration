c473a869b43e820ecba1eeb469a45769bd062434
visagegod <35296827+visagegod@users.noreply.github.com>
2018-01-10 13:00:10 +0000

    UpdateVisitRequest Commit

---
+++ src/main/java/com/qa/ons/UpdateVisitRequest.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class UpdateVisitRequest {
+	
+	private Visit visit;
+
+	public UpdateVisitRequest() {
+
+	}
+
+	public UpdateVisitRequest(Visit visit) {
+		super();
+		this.visit = visit;
+	}
+
+	@XmlAttribute
+	public String getVisit() {
+		return visit;
+	}
+
+	public void setVisit(String visit) {
+		this.visit = visit;
+	}
+
+}
