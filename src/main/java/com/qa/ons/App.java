9ad87511a33f3f1996358d385002f6aa9a509953
Thomas Poot <thomas.poot@qa.com>
2018-01-10 14:30:03 +0000

    Moved the XML conversion logic to the UpdateVisitRequest class

--- src/main/java/com/qa/ons/App.java
+++ src/main/java/com/qa/ons/App.java
@@ -1,31 +1,16 @@
 package com.qa.ons;
 
-import java.io.FileOutputStream;
 import java.util.ArrayList;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
+import javax.xml.bind.JAXBException;
 
 import com.qa.ons.data.*;
 
-/**
- * Hello world!
- *
- */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws JAXBException {
         Visit visit = new Visit(new Identity("Work", new User(), "Ref"), "Foo", new Property("Type", "Ref", "Name"));
         UpdateVisitRequest request = new UpdateVisitRequest(visit);
-        try {
-            JAXBContext contextObj = JAXBContext.newInstance(UpdateVisitRequest.class);
-
-            Marshaller marshallerObj = contextObj.createMarshaller();
-            marshallerObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            marshallerObj.marshal(request, new FileOutputStream("test.xml"));
-        } catch (Exception e) {
-            System.out.println("Failed!");
-        }
+        String xml = request.toXML();
+        System.out.println(xml);
     }
 }
