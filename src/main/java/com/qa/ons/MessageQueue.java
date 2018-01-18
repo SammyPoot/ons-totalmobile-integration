82d3921181aeb1024dbf375bdfc2b9204498e915
Thomas Poot <thomas.poot@qa.com>
2018-01-18 13:38:03 +0000

    Renamed the MessageQueue

--- src/main/java/com/qa/ons/MessageQueue.java
+++ src/main/java/com/qa/ons/MessageQueue.java
@@ -6,8 +6,11 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.SendUpdateVisitHeaderRequestMessage;
+import com.consiliumtechnologies.schemas.services.mobile._2009._03.messaging.SendUpdateVisitHeaderRequestMessageResponse;
+
 @Endpoint
-public class MessageQueue1 {
+public class MessageQueue {
     private static final String NAMESPACE_URI = "http://schemas.consiliumtechnologies.com/services/mobile/2009/03/messaging";
 
     @Autowired
@@ -24,6 +27,13 @@ public class MessageQueue1 {
         String response = new String("Foobar");
         // response.setCountry(countryRepository.findCountry(request.getName()));
 
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "SendUpdateVisitHeaderRequestMessage")
+    @ResponsePayload
+    public SendUpdateVisitHeaderRequestMessageResponse sendUpdateVisitHeaderRequestMessage(@RequestPayload SendUpdateVisitHeaderRequestMessage request) {
+        SendUpdateVisitHeaderRequestMessageResponse response = null;
         return response;
     }
 }
